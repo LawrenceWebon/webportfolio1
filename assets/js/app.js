c6cff8ba956ef5b7825f743ab46c2e8561eebfd3
Jose Lawrence Aquino <lawrencewebon@gmail.com>
2023-07-09 22:30:42 +0800

    Add aos

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,45 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+//FORMSPREE
+/* paste this line in verbatim */
+window.formbutton =
+  window.formbutton ||
+  function () {
+    (formbutton.q = formbutton.q || []).push(arguments);
+  };
+/* customize formbutton below*/
+formbutton("create", {
+  action: "https://formspree.io/f/mdorokny",
+  title: "How can we help?",
+  fields: [
+    {
+      type: "email",
+      label: "Email:",
+      name: "email",
+      required: true,
+      placeholder: "your@email.com",
+    },
+    {
+      type: "textarea",
+      label: "Message:",
+      name: "message",
+      placeholder: "What's on your mind?",
+    },
+    {type: "submit"},
+  ],
+  styles: {
+    title: {
+      backgroundColor: "gray",
+    },
+    button: {
+      backgroundColor: "gray",
+    },
+  },
+});
+
+// AOS
+AOS.init({
+  duration: 750,
+});
