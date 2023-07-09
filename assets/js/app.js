f610aedd71918eaff04848e8036921a7e390af83
Jose Lawrence Aquino <lawrencewebon@gmail.com>
2023-07-09 15:42:21 +0800

    Add new features

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,18 @@
-window.onscroll = function(){
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
-        document.getElementById('navbar').classList.add('scrolled');
-    } else{
-        document.getElementById('navbar').classList.remove('scrolled');
+// Navbar
+window.onscroll = function () {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 2) {
+    document.getElementById("navbar").classList.add("scrolled");
+  } else {
+    document.getElementById("navbar").classList.remove("scrolled");
+  }
+};
 
-    }
-}
+// Smooth scrolling anchor links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
